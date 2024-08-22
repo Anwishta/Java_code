ad30b1e9157e1a44d393f80ec9badccf28ea5a2b
Anwishta <anwishtaghosh@gmail.com>
2024-08-22 22:46:35 +0530

    pattern

--- pattern/pattern2.java
+++ pattern/pattern2.java
@@ -1,21 +1,21 @@
+package pattern;
 import java.util.Scanner;
 
-public class pattern6 {
+public class pattern2 {
     public static void main(String[] args) {
         System.out.println("Enter the number: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i =1; i<=n; i++){
-            for(int j = 1; j<=n-i; j++){
-                System.out.print("  ");
-            }
-            for(int j = 1; j<=n; j++){
-                if(j==1 || i == 1 || j==n || i==n){
+        sc.close();
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j<= n; j++){
+                if(i==1 || j==1 || i == n || j == n ){
                     System.out.print("* ");
                 }else{
                     System.out.print("  ");
                 }
             }System.out.println();
-        }sc.close();
+        }
     }
 }
+ 
--- pattern/pattern6.java
+++ pattern/pattern6.java
@@ -1,20 +1,22 @@
+package pattern;
 import java.util.Scanner;
 
-public class pattern2 {
+public class pattern6 {
     public static void main(String[] args) {
         System.out.println("Enter the number: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        sc.close();
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j<= n; j++){
-                if(i==1 || j==1 || i == n || j == n ){
+        for(int i =1; i<=n; i++){
+            for(int j = 1; j<=n-i; j++){
+                System.out.print("  ");
+            }
+            for(int j = 1; j<=n; j++){
+                if(j==1 || i == 1 || j==n || i==n){
                     System.out.print("* ");
                 }else{
                     System.out.print("  ");
                 }
             }System.out.println();
-        }
+        }sc.close();
     }
 }
- 
