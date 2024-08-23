0d0ed18f7f351a6b9e4d66a2baa049e76fe210da
Anwishta <anwishtaghosh@gmail.com>
2024-08-23 23:05:19 +0530

    getArea

---
+++ Demo.java
@@ -0,0 +1,26 @@
+class Shape {
+    double get_area(double r) {
+        return Math.PI * r * r; // Area of a circle
+    }
+    double get_area(double side_1, double side_2) {
+        return 0.5 * side_1 * side_2; // Area of a right triangle
+    }
+    double get_area(double side_1, double side_2, double side_3) {
+        double s = (side_1 + side_2 + side_3) / 2;
+        return Math.sqrt(s * (s - side_1) * (s - side_2) * (s - side_3)); // Area of a triangle
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        Shape shape = new Shape(); 
+        double circleArea = shape.get_area(5.3);
+        System.out.println("The area of the circle is: " + circleArea);
+
+//        double rightTriangleArea = shape.get_area(5, 9);
+//        System.out.println("The area of the right triangle is: " + rightTriangleArea);
+
+        double triangleArea = shape.get_area(5, 9, 12);
+        System.out.println("The area of the triangle is: " + triangleArea);
+    }
+}
