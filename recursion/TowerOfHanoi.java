9cb1339cc3fcf9160f7160de014b78652aacd12d
Anwishta <anwishtaghosh@gmail.com>
2024-09-01 23:41:23 +0530

    ...

--- recursion/TowerOfHanoi.java
+++ recursion/TowerOfHanoi.java
@@ -2,13 +2,14 @@ package recursion;
 public class TowerOfHanoi {
     public static void tower(int n, String src, String help, String dest){
         if (n==1){
-            System.out.println("transfer"+n+"from"+src+"to"+dest);
+            System.out.println("transfer "+n+" from "+src+" to "+dest);
+            return;
         }
         tower(n-1, src, dest, help);
-        System.out.println("transfer"+n+"from"+src+"to"+dest);
+        System.out.println("transfer "+n+" from "+src+" to "+help);
         tower(n-1, help, src, dest);
     }
     public static void main(String[] args) {
-        tower(2,  "A", "B", "C");
+        tower(3,  "A", "B", "C");
     }
 }
