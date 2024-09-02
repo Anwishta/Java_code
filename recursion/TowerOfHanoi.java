925c6972feaa35ead2fb408d6128be315ac08d76
Anwishta <anwishtaghosh@gmail.com>
2024-09-02 23:24:32 +0530

    Tower of Hanoi

--- recursion/TowerOfHanoi.java
+++ recursion/TowerOfHanoi.java
@@ -6,7 +6,7 @@ public class TowerOfHanoi {
             return;
         }
         tower(n-1, src, dest, help);
-        System.out.println("transfer "+n+" from "+src+" to "+help);
+        System.out.println("transfer "+n+" from "+src+" to "+dest);
         tower(n-1, help, src, dest);
     }
     public static void main(String[] args) {
