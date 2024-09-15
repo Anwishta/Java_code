e45ec91c642198d16ea1e141a64807aeba2f06a3
Anwishta <anwishtaghosh@gmail.com>
2024-09-15 22:24:50 +0530

    keyboard combination

--- recursion/KeyboardCombination.java
+++ recursion/KeyboardCombination.java
@@ -2,16 +2,20 @@ package recursion;
 
 public class KeyboardCombination {
     public static String arr[] = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs", "wxyz"};
-    public static void printCombinatiob(int idx, String s, String res){
-        if(idx==s.length()){
-            System.out.println(res);
+    public static void printCombination(String str, int idx, String combination){
+        if(idx==str.length()){
+            System.out.println(combination);
             return;
         }
-        
+        char curr = str.charAt(idx);
+        String mapping = arr[curr-'0'];
+        for (int i = 0; i < mapping.length(); i++) {
+            printCombination(str, idx+1, combination+mapping.charAt(i));
             
         }
-    }
+            
+        }
     public static void main(String[] args) {
-        
+        printCombination("23", 0, "");
     }
 }
