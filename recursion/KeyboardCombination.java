e493a55a875ef095f315a0587a705b3f6fd179db
Anwishta <anwishtaghosh@gmail.com>
2024-09-15 22:17:33 +0530

    keyboard combination

---
+++ recursion/KeyboardCombination.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class KeyboardCombination {
+    public static String arr[] = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs", "wxyz"};
+    public static void printCombinatiob(int idx, String s, String res){
+        if(idx==s.length()){
+            System.out.println(res);
+            return;
+        }
+        
+            
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
