5b2bf36b48afee17a9c6574e1ca6b1fe82ac69ee
Anwishta <anwishtaghosh@gmail.com>
2024-09-15 22:17:09 +0530

    permutation

---
+++ recursion/Permutation.java
@@ -0,0 +1,14 @@
+package recursion;
+
+
+public class Permutation {
+    public static void print(String s, int idx, String newString){
+        if(idx==s.length()){
+            System.out.println(newString);
+            return;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
