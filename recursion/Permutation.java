4a6e6e0e08c69b8d808ecec4a0a6549dab29f1c8
Anwishta <anwishtaghosh@gmail.com>
2024-09-16 23:04:09 +0530

    permutation

--- recursion/Permutation.java
+++ recursion/Permutation.java
@@ -2,13 +2,18 @@ package recursion;
 
 
 public class Permutation {
-    public static void print(String s, int idx, String newString){
-        if(idx==s.length()){
-            System.out.println(newString);
+    public static void print(String s, String permutation){
+        if(s.length()==0){
+            System.out.println(permutation);
             return;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char curr = s.charAt(i);
+            String newString = s.substring(0, i) + s.substring(i+1);
+            print(newString, permutation+curr);
         }
     }
     public static void main(String[] args) {
-        
+        print("abc", "");
     }
 }
