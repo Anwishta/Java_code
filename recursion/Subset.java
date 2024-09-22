e2c86012dc66a0171566e40c36e0e8dbe7e53330
Anwishta <anwishtaghosh@gmail.com>
2024-09-22 23:39:04 +0530

    ...

--- recursion/Subset.java
+++ recursion/Subset.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 class Subset{
     public static void printsubset(int idx, Arraylist<Integer> set ){
 
-        set.add()
+        set.add();
+        set.remove();
     }
     public static void main(String[] args) {
         ArrayList<Integer> set = new ArrayList<>();
