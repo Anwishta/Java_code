01a776e1e8d121d50bb33f3f782ce768701d6130
Anwishta <anwishtaghosh@gmail.com>
2024-09-29 11:29:30 +0530

    Subset

--- recursion/Subset.java
+++ recursion/Subset.java
@@ -1,15 +1,30 @@
-package recursion;
+
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class Subset{
-    public static void printsubset(int idx, Arraylist<Integer> set ){
+    public static void printSet(ArrayList<Integer> set){
+        System.out.println(set);
+    }
 
-        set.add();
-        set.remove();
+    public static void subset(int n, ArrayList<Integer> set ){
+        if(n==0){
+            printSet(set);
+            return;
+        }
+        set.add(n);
+        subset(n-1, set);
+        set.remove(set.size()-1);
+        subset(n-1, set);
     }
     public static void main(String[] args) {
         ArrayList<Integer> set = new ArrayList<>();
+        System.out.println("Enter the number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        subset(n, set);
+        sc.close();;
     }
 }
 
