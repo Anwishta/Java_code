7fb1bbb81f9c40dd67c4050ffb8321134841e064
Anwishta <anwishtaghosh@gmail.com>
2024-11-15 22:17:45 +0530

    delete from last n position

--- LinkedList/DeleteFromLastNPos.java
+++ LinkedList/DeleteFromLastNPos.java
@@ -1,3 +1,5 @@
+//count the position from end to delete the node
+
 public class DeleteFromLastNPos {
     class Node{
         String data;
@@ -8,7 +10,7 @@ public class DeleteFromLastNPos {
         }
     }
     Node head = null;
-    public void insertFirst(){
+    public void insertFirst(String data){
         Node newNode = new Node(data);
         if(head == null){
             head = newNode;
@@ -27,7 +29,7 @@ public class DeleteFromLastNPos {
         }
         currNode.next = newNode;
     }
-    public void deleteEnd(int n){
+    public Node deleteEnd(int n){
         Node curr = head;
         int size = 0;
         while(curr != null){
@@ -36,11 +38,14 @@ public class DeleteFromLastNPos {
         }
         int indexToDelete = size - n;
         curr = head;
-        while(i<size){
+        int i=1;
+        while(i<indexToDelete){
             curr = curr.next;
+            i++;
 
         }
         curr.next = curr.next.next;
+        return head;
     }
     public void print(){
         Node currNode = head;
@@ -56,6 +61,16 @@ public class DeleteFromLastNPos {
     }
 
     public static void main(String[] args) {
+        DeleteFromLastNPos del = new DeleteFromLastNPos();
+        del.insertFirst("1");
+        del.insertEnd("2");
+        del.insertEnd("3");
+        del.insertEnd("4");
+        del.print();
+        del.deleteEnd(1);
+        System.out.println();
+        del.print();
+
         
     }
 }
