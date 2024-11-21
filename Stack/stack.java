08281784e2de855eba200bf397fde6dc23ad0683
Anwishta <anwishtaghosh@gmail.com>
2024-11-21 20:18:37 +0530

    Stack

--- Stack/stack.java
+++ Stack/stack.java
@@ -10,10 +10,7 @@ public class stack {
     static Node head = null;
     static class impleStack{
         public static boolean isEmpty(){
-            if(head == null){
-                return true;
-            }
-            return false;
+            return head == null;
         }
         public static void push(String data){
             Node newNode = new Node(data);
@@ -24,8 +21,33 @@ public class stack {
             newNode.next = head;
             head = newNode;
         }
+        public static String pop(){
+            if(isEmpty()){
+                return "Empty";
+            }
+            String data = head.data;
+            head = head.next;
+            return data;
+        }
+        public static String peek(){
+            if(isEmpty()){
+                return "Empty";
+            }
+            return head.data;
+        }
     }
     public static void main(String[] args) {
-        
+        impleStack s = new impleStack();
+        s.push("1");
+        s.push("2");
+        s.push("3");
+        s.push("4");
+        s.push("5");
+        System.out.println(s.peek());
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+            
+        }
+
     }
 }
