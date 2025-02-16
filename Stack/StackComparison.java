989dc64a5f8486a2adbd41aee0fe002723aacb65
Anwishta <anwishtaghosh@gmail.com>
2025-02-16 18:52:04 +0530

    graph

---
+++ Stack/StackComparison.java
@@ -0,0 +1,14 @@
+class Stack{
+    private int stackArray[];
+    private int top;
+    private int maxsize;
+
+    public Stack(int size){
+        maxsize = size;
+        top = -1;
+        stackArray = new int[maxsize];
+    }
+    public void push(int value){
+        if()
+    }
+}
